208fa12a0b5bb3d837f3e93e1341e1fcdf07f209
Sabaris <sabaris.elango@gmail.com>
2024-12-30 02:17:23 +0530

    Additions for Sign Up page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,13 @@
 exports.getIndexPage = (req, res) => {
   res.render("index");
 };
+
+exports.getSignUpPage = (req, res) => {
+  res.render("signUp");
+};
+
+exports.postSignUp = (req, res) => {
+  // lot of things to do
+  // express-validator
+  // prisma instead of direct postgresql db
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,5 +2,7 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getIndexPage());
+indexRouter.get("/signUp", indexController.getSignUpPage());
+indexRouter.post("/signUp", indexController.postSignUp());
 
 module.exports = indexRouter;
